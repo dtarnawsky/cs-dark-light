8f08f096650566d636f00714ac36a4f20bbb5374
damian.ionic <damian@ionic.io>
2024-08-07 15:14:21 -0700

    Initial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.dark.light',
+  appName: 'cs-dark-light',
+  webDir: 'www'
+};
+
+export default config;
