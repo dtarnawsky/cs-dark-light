97b7ee2276d8ed0f32a9ec3a9091080255bf0402
damian.ionic <damian@ionic.io>
2024-08-13 07:44:19 -0700

    feat: add dark mode detection and status bar styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { NavigationBar } from '@hugotomazi/capacitor-navigation-bar';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +13,22 @@ import { NavigationBar } from '@hugotomazi/capacitor-navigation-bar';
 export class AppComponent implements OnInit {
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
     const darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     const color = darkMode ? '#003300' : '#00FF00';
     NavigationBar.setColor({ color: color, darkButtons: true });
+
+    this.styleStatusBar(darkMode);
+    window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', ({ matches }) => {
+        this.styleStatusBar(matches);
+      });
+  }
+
+  private async styleStatusBar(darkMode: boolean) {
+    StatusBar.setStyle({ style: darkMode ? Style.Dark : Style.Light });
+    if (Capacitor.getPlatform() === 'android') {
+      await StatusBar.setBackgroundColor({ color: darkMode ? '#000000' : '#FFFFFF' });
+    }
   }
 }
